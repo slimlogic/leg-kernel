c18f451b4596c175e04eabe264c7263395686cc3
Graeme Gregory <graeme.gregory@linaro.org>
2013-06-13 10:14:32 +0100

    ARM64: ACPI: fix email address for Al Stone
    Change to @linaro.org for Al Stone email address
    
    Signed-off-by: Graeme Gregory <graeme.gregory@linaro.org>

--- arch/arm64/include/asm/acpi.h
+++ arch/arm64/include/asm/acpi.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2013, Al Stone <ahs3@redhat.com>
+ *  Copyright (C) 2013, Al Stone <al.stone@linaro.org>
  *
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *
