8f0e24d5ff84300904fe1e67eab77a605b4884b7
Hanjun Guo <hanjun.guo@linaro.org>
2013-07-03 19:15:23 +0800

    ARM64 / ACPI: Introduce arch_register/unregister_cpu() for arm64
    Introduce arch_register/unregister_cpu() for ACPI driver, which can
    register/unregister cpu interfaces in /sys/devices/system/cpu/cpux.
    
    Signed-off-by: Hanjun Guo <hanjun.guo@linaro.org>

--- arch/arm64/kernel/topology.c
+++ arch/arm64/kernel/topology.c
@@ -18,3 +18,29 @@ void arch_fix_phys_package_id(int num, u32 slot)
 }
 EXPORT_SYMBOL_GPL(arch_fix_phys_package_id);
 
+#ifdef CONFIG_HOTPLUG_CPU
+int __ref arch_register_cpu(int cpu)
+{
+	struct cpuinfo_arm *cpuinfo = &per_cpu(cpu_data, cpu);
+
+	/* BSP cann't be taken down on arm */
+	if (cpu)
+		cpuinfo->cpu.hotpluggable = 1;
+
+	return register_cpu(&cpuinfo->cpu, cpu);
+}
+EXPORT_SYMBOL(arch_register_cpu);
+
+void arch_unregister_cpu(int cpu)
+{
+	unregister_cpu(&per_cpu(cpu_data, cpu).cpu);
+}
+EXPORT_SYMBOL(arch_unregister_cpu);
+#else /* CONFIG_HOTPLUG_CPU */
+
+static int __init arch_register_cpu(int cpu)
+{
+	return register_cpu(&per_cpu(cpu_data, cpu).cpu, cpu);
+}
+#endif /* CONFIG_HOTPLUG_CPU */
+
