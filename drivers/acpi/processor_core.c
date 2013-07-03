73c1c061aab043fd1f5f32ab0f3a66c9e42bbdf9
Hanjun Guo <hanjun.guo@linaro.org>
2013-07-03 19:15:20 +0800

    ARM64 / ACPI: Get logic cpu id to see if the cpu is a new added cpu
    Since enabled cpus were allocated logic cpu id to them and mapped to the
    APIC id when boot, we can get the cpu id in the ACPI driver, the cpu is
    a new added cpu if cpu id is -1, then we can hot-add it later.
    
    Signed-off-by: Hanjun Guo <hanjun.guo@linaro.org>

--- drivers/acpi/processor_core.c
+++ drivers/acpi/processor_core.c
@@ -190,6 +190,9 @@ exit:
 
 int acpi_get_cpuid(acpi_handle handle, int type, u32 acpi_id)
 {
+#ifdef CONFIG_SMP
+	int i;
+#endif
 	int apic_id = -1;
 
 	apic_id = map_mat_entry(handle, type, acpi_id);
@@ -222,31 +225,16 @@ int acpi_get_cpuid(acpi_handle handle, int type, u32 acpi_id)
 			return apic_id;
 	}
 
-#if defined(CONFIG_ARM) || defined(CONFIG_ARM64)
-
-	/*
-	 * BOZO: need to abstract this out to have it make sense --
-	 * it's not that ARM has no equivalent, it's that apic_id is
-	 * arch-specific
-	 */
-
-#else
-
 #ifdef CONFIG_SMP
-	{
-		int i;
-		for_each_possible_cpu(i) {
-			if (cpu_physical_id(i) == apic_id)
-				return i;
-		}
+	for_each_possible_cpu(i) {
+		if (cpu_physical_id(i) == apic_id)
+			return i;
 	}
 #else
 	/* In UP kernel, only processor 0 is valid */
 	if (apic_id == 0)
 		return apic_id;
 #endif
-
-#endif /* defined(CONFIG_ARM) || defined(CONFIG_ARM64) */
 
 	return -1;
 }
